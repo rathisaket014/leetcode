804bf3059230a5f117ef5a17a1d9d8f8982cd36e
Ice <ice.westwind@gmail.com>
2025-02-01 22:37:14 +0530

    Time: 1558 ms (37.51%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,9 +1,17 @@
 /* Write your PL/SQL query statement below */
-SELECT 
-    p.product_id,
-    ROUND(COALESCE(SUM(u.units * p.price) / SUM(u.units), 0), 2) AS average_price
-FROM Prices p
-LEFT JOIN UnitsSold u 
-    ON p.product_id = u.product_id 
-    AND u.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY p.product_id;
+select p.product_id, 
+    ROUND(COALESCE(SUM(u.units * p.price) / SUM(u.units), 0), 2) as average_price
+from Prices p
+left join UnitsSold u 
+    on p.product_id = u.product_id
+    and u.purchase_date between p.start_date and p.end_date
+group by p.product_id; 
+
+-- SELECT 
+--     p.product_id,
+--     ROUND(COALESCE(SUM(u.units * p.price) / SUM(u.units), 0), 2) AS average_price
+-- FROM Prices p
+-- LEFT JOIN UnitsSold u 
+--     ON p.product_id = u.product_id 
+--     AND u.purchase_date BETWEEN p.start_date AND p.end_date
+-- GROUP BY p.product_id;
