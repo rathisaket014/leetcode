bae573ede33fafc4b57341c33f4975f682873035
Ice <ice.westwind@gmail.com>
2025-02-06 23:38:57 +0530

    Time: 179 ms (85.26%) | Memory: 68.4 MB (76.92%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.java
+++ 1364-tuple-with-same-product/tuple-with-same-product.java
@@ -15,7 +15,8 @@ class Solution {
 
         for (int f : freq.values()) {
             if (f > 1) {
-                res += (f * (f - 1) / 2) * 8;
+                res += (f * (f - 1) / 2) * 8; // cuz for 2 pairs 8 tuples can be defined so * 8
+            // Suppose a product appears k times (i.e., there are k pairs that produce this product). We can choose any two distinct pairs out of these k pairs in k C 2 ways.
             }
         }
 
