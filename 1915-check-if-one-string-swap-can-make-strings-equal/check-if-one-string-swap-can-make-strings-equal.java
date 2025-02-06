637e02072275f4f9496abae63b05f1d225de0dc8
Ice <ice.westwind@gmail.com>
2025-02-06 23:20:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (76.91%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -1,31 +1,21 @@
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
-        if (s1.equals(s2)) {
-            return true;
-        }
-        
-        // Use a list to store indices where s1 and s2 differ.
-        int[] diff = new int[2];
-        int diffCount = 0;
-        
-        // Loop over the characters of the strings.
+        if (s1.equals(s2)) return true;
+
+        int[] arr = new int[2];
+        int cnt = 0;
+
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
-                // If more than 2 differences, return false.
-                if (diffCount == 2) {
-                    return false;
-                }
-                diff[diffCount] = i;
-                diffCount++;
+                if  (cnt == 2) return false;
+                arr[cnt] = i;
+                cnt++;
             }
         }
+
+        if (cnt != 2) return false;   
         
-        // If the number of differences is not exactly 2, return false.
-        if (diffCount != 2) {
-            return false;
-        }
-        
-        // Check if swapping the two differing indices in one of the strings would result in equality.
-        return s1.charAt(diff[0]) == s2.charAt(diff[1]) && s1.charAt(diff[1]) == s2.charAt(diff[0]);
+        return s1.charAt(arr[0]) == s2.charAt(arr[1]) && s1.charAt(arr[1]) == s2.charAt(arr[0]);
+    
     }
 }
