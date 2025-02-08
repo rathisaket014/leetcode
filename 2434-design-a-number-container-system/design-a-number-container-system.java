e53d0cbf3e749a109e136ad3811591ef77565b3f
Ice <ice.westwind@gmail.com>
2025-02-08 22:53:20 +0530

    Time: 84 ms (47.95%) | Memory: 100.3 MB (32.88%) - LeetSync

---
+++ 2434-design-a-number-container-system/design-a-number-container-system.java
@@ -0,0 +1,47 @@
+class NumberContainers {
+
+    private Map<Integer, Integer> idxToNum;
+
+    private Map<Integer, TreeSet<Integer>> numToIdx;
+
+    public NumberContainers() {
+        idxToNum = new HashMap<>();
+        numToIdx = new HashMap<>();
+    }
+    
+    public void change(int index, int number) {
+        if (idxToNum.containsKey(index)) {
+            int oldno = idxToNum.get(index);
+            TreeSet<Integer> oldset = numToIdx.get(oldno);
+            oldset.remove(index);
+
+            if (oldset.isEmpty()) {
+                numToIdx.remove(oldno);
+            }
+        }
+
+        idxToNum.put(index, number);
+
+        TreeSet<Integer> set = numToIdx.get(number);
+        if (set == null) {
+            set = new TreeSet<>();
+            numToIdx.put(number, set);
+        }
+        set.add(index);
+    }
+    
+    public int find(int number) {
+        if (numToIdx.containsKey(number) && !numToIdx.get(number).isEmpty()) {
+            return numToIdx.get(number).first();
+        }
+
+        return -1;
+    }
+}
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers obj = new NumberContainers();
+ * obj.change(index,number);
+ * int param_2 = obj.find(number);
+ */
