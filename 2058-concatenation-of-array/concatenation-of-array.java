64007c51553cf4fc32297c1f737487b9780d9fcf
Ice <ice.westwind@gmail.com>
2025-02-23 22:05:48 +0530

    Time: 1 ms (95.96%) | Memory: 45.3 MB (64.21%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.java
+++ 2058-concatenation-of-array/concatenation-of-array.java
@@ -2,11 +2,15 @@ class Solution {
     public int[] getConcatenation(int[] nums) {
         int n = nums.length;
         int[] ans = new int[2 * n];
-        int i = 0;
-        for (int num : nums) {
-            ans[i] = num;
-            ans[i + n] = num;
-            i++;
+        // int i = 0;
+        // for (int num : nums) {
+        //     ans[i] = num;
+        //     ans[i + n] = num;
+        //     i++;
+        // }
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
         }
 
         return ans;
