436aa1a7378eae01ba6a832e96f70594634c4f50
Ice <ice.westwind@gmail.com>
2025-03-16 00:00:46 +0530

    Time: 39 ms (88.04%) | Memory: 53.3 MB (30.38%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -4,10 +4,15 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    let res = []
-    for (let i = 0; i < arr.length; i++) {
-        res.push(fn(arr[i], i))
-    }
+    // let res = []
+    // for (let i = 0; i < arr.length; i++) {
+    //     res.push(fn(arr[i], i))
+    // }
 
+    // return res
+    const res = new Array(arr.length)
+    for (let i = 0; i < arr.length; i++) {
+        res[i] = fn(arr[i], i)
+    }
     return res
 };
