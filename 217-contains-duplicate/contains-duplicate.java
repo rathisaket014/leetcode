8a0cf4cefecb9f1610ac106655dafbcd8688597a
Ice <ice.westwind@gmail.com>
2025-03-25 22:15:56 +0530

    Time: 20 ms (23.33%) | Memory: 55.3 MB (85.94%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,14 +1,24 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer, Integer> set = new HashMap<>();
+        // int n = nums.length;
+        // HashMap<Integer, Integer> set = new HashMap<>();
 
-        for (int num : nums) {
-            set.put(num, set.getOrDefault(num, 0) + 1);
-        }
+        // for (int num : nums) {
+        //     set.put(num, set.getOrDefault(num, 0) + 1);
+        // }
 
-        for (int key : set.keySet()) {
-            if (set.get(key) > 1) {
+        // for (int key : set.keySet()) {
+        //     if (set.get(key) > 1) {
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
                 return true;
             }
         }
