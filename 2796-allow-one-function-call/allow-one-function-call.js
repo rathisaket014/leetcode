9a1fab93c2adfb119e9d4435f9ffe1f89b43a1e6
Ice <ice.westwind@gmail.com>
2025-03-28 22:12:53 +0530

    Time: 40 ms (80.42%) | Memory: 53.7 MB (18.74%) - LeetSync

---
+++ 2796-allow-one-function-call/allow-one-function-call.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let flag = false;
+    let res;
+    return function(){
+        if (!flag) {
+            flag = true
+            res = fn.apply(this, arguments)
+            return res
+        } else {
+            return undefined
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
