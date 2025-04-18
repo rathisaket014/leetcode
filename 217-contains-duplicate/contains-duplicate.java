4d702ccac922ba2ed9d845f238926a898904b6be
Ice <ice.westwind@gmail.com>
2025-04-18 10:32:20 +0530

    Time: 14 ms (41.05%) | Memory: 58.4 MB (37.32%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -15,12 +15,25 @@ class Solution {
 
         // return false;
 
-        Arrays.sort(nums);
+        // Arrays.sort(nums); O(nlogn)
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
+        // for (int i = 1; i < nums.length; i++) {
+        //     if (nums[i] == nums[i - 1]) {
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+
+        // O(n)
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
                 return true;
             }
+
+            set.add(num);
         }
 
         return false;
