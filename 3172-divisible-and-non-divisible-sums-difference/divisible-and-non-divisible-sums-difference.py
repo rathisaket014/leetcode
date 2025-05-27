00b06c63cbd0bd5624cfd4c5358e74a243fc1244
Ice <ice.westwind@gmail.com>
2025-05-27 14:16:30 +0530

    Time: 1 ms (71.11%) | Memory: 17.7 MB (90.64%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.py
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.py
@@ -3,11 +3,9 @@ class Solution:
         num1, num2 = 0, 0
 
         for i in range(1, n + 1):
-            if (i % m != 0):
-                num1 += i
-
-        for i in range(1, n + 1):
             if (i % m == 0):
                 num2 += i
+            else:
+                num1 += i
 
         return num1 - num2
