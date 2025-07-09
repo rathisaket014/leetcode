e2891e682d291a08f1f1aaad0765d8507b633639
Ice <ice.westwind@gmail.com>
2025-07-10 00:33:10 +0530

    Time: 1 ms (99.75%) | Memory: 46.1 MB (48.64%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,28 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        /*
-        0 1 0 3 12
-        i j
-        1 0 0 3 12
-          i   j
-        1 3 0 0 12
-            i   j
-        1 3 12 0 0
-         */
+        int idx = 0;
 
-        if (nums.length == 1) {
-            return;
+        for (int n : nums) {
+            if (n != 0) {
+                nums[idx++] = n; // nums[idx] = n then idx++;
+            }
         }
 
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[i] != 0) i++;
-            if (nums[j] == 0 && j < nums.length - 1) j++;
-            if (nums[i] == 0 && nums[j] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-            }
+        while (idx < nums.length) {
+            nums[idx++] = 0;
         }
     }
 }
