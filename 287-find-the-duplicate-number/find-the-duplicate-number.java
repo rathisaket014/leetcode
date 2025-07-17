3de639c2a0c8b3feccea4cbf7e3664f0decd507f
Ice <ice.westwind@gmail.com>
2025-07-17 23:36:52 +0530

    Time: 4 ms (88.53%) | Memory: 58.4 MB (49.02%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slw = nums[0];
+        int fst = nums[0];
+
+        do {
+            slw = nums[slw];
+            fst = nums[nums[fst]];
+        } while(slw != fst);
+
+        slw = nums[0];
+
+        while(slw != fst) {
+            slw = nums[slw];
+            fst = nums[fst];
+        }
+
+        return slw;
+    }
+}
