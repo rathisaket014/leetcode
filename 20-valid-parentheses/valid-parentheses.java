afae0c6241b8d993b38e5a02020ea97a1a763928
Ice <ice.westwind@gmail.com>
2025-07-17 23:54:21 +0530

    Time: 2 ms (96.71%) | Memory: 41.9 MB (31.53%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(' || c == '{' || c == '[') {
+                stk.push(c);
+            } else {
+                if (stk.isEmpty()) return false;
+
+                char top = stk.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')
+                ) {
+                    return false;
+                }
+            }
+        }
+
+        return stk.isEmpty();
+    }
+}
