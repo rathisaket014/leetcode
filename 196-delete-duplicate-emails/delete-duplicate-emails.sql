8e511eebc28670189b6bf03f3f49a4cdf7e4a9fe
Ice <ice.westwind@gmail.com>
2025-09-02 20:28:12 +0530

    Time: 250 ms (51.55%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,5 +1,11 @@
-DELETE p1
-FROM Person p1
-JOIN Person p2
-  ON p1.email = p2.email 
- AND p1.id > p2.id; # this means we keep the row with smallest id
+-- Write your PostgreSQL query statement below
+DELETE FROM Person
+WHERE id IN (
+    SELECT id
+    FROM (
+        SELECT id,
+               ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS rn
+        FROM Person
+    )
+    WHERE rn > 1
+);
