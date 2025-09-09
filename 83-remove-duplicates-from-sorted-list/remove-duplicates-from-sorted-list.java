782e1acf797b70f2081b0a69556a5e1b80e1b606
Ice <ice.westwind@gmail.com>
2025-09-09 23:29:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (43.52%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -14,12 +14,24 @@ class Solution {
 
         ListNode curr = head;
 
-        while (curr != null && curr.next != null) {
-            if (curr.val == curr.next.val) {
-                curr.next = curr.next.next;
-            } else {
-                curr = curr.next;
+        // while (curr != null && curr.next != null) {
+        //     if (curr.val == curr.next.val) {
+        //         curr.next = curr.next.next;
+        //     } else {
+        //         curr = curr.next;
+        //     }
+        // }
+
+        while (curr != null) {
+            ListNode temp = curr.next;
+
+            while (temp != null && temp.val == curr.val) {
+                temp = temp.next;
             }
+
+            curr.next = temp;
+
+            curr = temp;
         }
 
         return head;
