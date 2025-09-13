b0bc321329fcfc6b0a679b82bc9424465f25d77c
Ice <ice.westwind@gmail.com>
2025-09-13 22:46:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (31.76%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,7 +1,21 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words = s.trim().split("\\s+");
+        // String[] words = s.trim().split("\\s+");
 
-        return words[words.length - 1].length();
+        // return words[words.length - 1].length();
+
+        int n = s.length() - 1;
+        int len = 0;
+
+        while (n >= 0 && s.charAt(n) == ' ') {
+            n--;
+        }
+
+        while (n >= 0 && s.charAt(n) != ' ') {
+            len++;
+            n--;
+        }
+
+        return len;
     }
 }
