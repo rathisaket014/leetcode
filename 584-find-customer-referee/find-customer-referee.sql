9552181f4b961ef3e0900e626671e7d4a3f432de
Ice <ice.westwind@gmail.com>
2025-09-22 18:37:20 +0530

    Time: 452 ms (73.13%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,5 +1,4 @@
-/* Write your PL/SQL query statement below */
+# Write your MySQL query statement below
 select name
 from Customer
-where referee_id <> 2 or referee_id is null;
-/* <> : not equal to and check for null vals by is null */
+where referee_id != 2 or referee_id is null
