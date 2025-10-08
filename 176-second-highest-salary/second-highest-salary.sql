650c24f0d3cf1d943b32c0fec404bc2124f0f54e
Ice <ice.westwind@gmail.com>
2025-10-08 22:43:07 +0530

    Time: 293 ms (26.02%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,9 +1,15 @@
-select coalesce(
-    (
-        select distinct salary
-        from Employee
-        order by salary desc
-        limit 1 offset 1
-    ),
-    null
-) as SecondHighestSalary;
+-- select coalesce(
+--     (
+--         select distinct salary
+--         from Employee
+--         order by salary desc
+--         limit 1 offset 1
+--     ),
+--     null
+-- ) as SecondHighestSalary;
+
+select max(salary) as SecondHighestSalary
+from Employee
+where salary < (
+    select max(salary) from employee
+);
